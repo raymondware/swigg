c0e1272c5cb6a1c2a16e0b40199a164c46dad6f9
Raymond Ware <raymondware.design@gmail.com>
2020-02-07 23:16:57 -0800

    Change export structure

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
-import { Button } from 'swigg'
+import { Button, ProjectCard } from 'swigg'
 
 export default class App extends Component {
   render () {
     return (
       <div>
         <Button />
+        <ProjectCard />
       </div>
     )
   }
