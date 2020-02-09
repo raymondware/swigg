c5ac70a9afb8f4e982bae3f0554166e40c1274cc
Raymond Ware <raymondware.design@gmail.com>
2020-02-09 11:43:54 -0800

    Adjust example

--- example/src/App.js
+++ example/src/App.js
@@ -11,13 +11,8 @@ export default class App extends Component {
     return (
       <React.Fragment>
         <MainNav />
-        <Button>Click me</Button>
         <AutoGrid bg="#212121" minWidth="250px" spacing="45px" padding="45px">
-          <ProjectCard maxWidth="250px" title="Party City" bgOverlay="linear-gradient(45deg, steelblue, darkorchid)">
-            <Button>
-              Test
-            </Button>
-          </ProjectCard>
+          <ProjectCard maxWidth="250px" title="Party City" bgOverlay="linear-gradient(45deg, steelblue, darkorchid)" />
           <ProjectCard maxWidth="250px" title="Another Component" bgOverlay="grey" />
           <ProjectCard maxWidth="250px" title="This lib is sick" bgOverlay="darkkhaki" />
           <ProjectCard maxWidth="250px" title="Pretty dope" bgOverlay="aquamarine" />
@@ -42,6 +37,8 @@ export default class App extends Component {
           <ProjectCard maxWidth="650px" title="Dynamic" bgOverlay="steelblue" bgImage="https://images.unsplash.com/photo-1581078253766-5876a6cabd72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
           <ProjectCard maxWidth="650px" title="Dynamic" link="https://www.google.com" children="This is the description" techList={["party", "people", "tags"]} bgOverlay="tomato" bgImage="https://images.unsplash.com/photo-1581094935665-144e76b36c25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
         </AutoGrid>
+
+        <Button>Click me</Button>
 
         <MasonryGrid bg="#fcfcfc" padding="45px" spacing="25px" colSize="350px" items={[
           {
