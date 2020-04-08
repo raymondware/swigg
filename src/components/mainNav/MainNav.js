8d717f167e9144650637f3fd097ed0a0d92a33c1
Raymond Ware <raymondware.design@gmail.com>
2020-04-07 21:24:56 -0700

    Mobile menu styles

--- src/components/mainNav/MainNav.js
+++ src/components/mainNav/MainNav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
@@ -17,30 +17,56 @@ const Nav = styled.nav`
     margin: 0;
     padding: 0;
 
+    button {
+      display: none;
+      
+      @media only screen and (max-width: 800px) {
+        display: block;
+      }
+    }
+
     ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
+      background: ${props => props.bg};
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      transition: visibility 0s, opacity 0.5s linear;
 
-        li {
-          list-style-type: none;
+      li {
+        list-style-type: none;
+        color: ${props => props.fontColor};
+        font-size: 1.2em;
+
+        a {
+          display: block;
+          text-decoration: none;
           color: ${props => props.fontColor};
-          font-size: 1.2em;
+          padding: 1em;
+          transition: all .4s;
 
-          a {
-            display: block;
-            text-decoration: none;
-            color: ${props => props.fontColor};
-            padding: 1em;
-            transition: all .4s;
-
-            &:hover {
-              background: red;
-              color: #fff;
-            }
+          &:hover {
+            background: red;
+            color: #fff;
           }
         }
+      }
+      
+      @media only screen and (max-width: 800px) {
+        width: 100%;
+        position: absolute;
+        top: 25px;
+        left: 0;
+        margin: 0 auto;
+        flex-direction: column;
+        display: ${elm => elm.toggle ? 'block' : 'none'};
+        z-index: 8888;
+        
+        li {
+          display: block;
+          margin: 0;
+        }
+      }
     }
 `
 
@@ -50,11 +76,16 @@ const Nav = styled.nav`
     - logo image
 */
 const MainNav = props => {
+  const [toggle, setToggle] = useState(false)
+
+  const triggerToggle = () => setToggle(!toggle)
+
   return (
     <MainNavWrap {...props}>
       <Logo />
 
-      <Nav {...props}>
+      <Nav {...props} toggle={toggle}>
+        <button onClick={triggerToggle}>+ {toggle.toString()}</button>
         {props.children}
       </Nav>
     </MainNavWrap>
---
+++ src/hooks/useWindowResize.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+// https://usehooks.com/useWindowSize/
+const useWindowSize = () => {
+  const isClient = typeof window === 'object'
+
+  const getSize = () => {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined
+    }
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize)
+
+  useEffect(() => {
+    if (!isClient) {
+      return false
+    }
+
+    const handleResize = () => {
+      setWindowSize(getSize())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowSize
+}
+
+export default useWindowSize
